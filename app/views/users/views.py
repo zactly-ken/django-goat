ca88a0169a75936514c553077740b06fee72cfec
zactly-ken <30415618+zactly-ken@users.noreply.github.com>
2024-04-30 11:25:16 -0400

    Update views.py

--- app/views/users/views.py
+++ app/views/users/views.py
@@ -60,42 +60,42 @@ def account_settings(request, user_id):
                       context=context)
 
 
-# @require_http_methods(["GET", "POST", "PUT", "DELETE"])
-# @user_is_authenticated
-# def user_view(request, user_id):
-#     if request.method == "POST":
-#         form = request.POST
-#         if not form:
-#             return HttpResponse("User " + str(user_id) + "POST")
-#         user_id_form = form['user_id']
-#         table_name = User.objects.model._meta.db_table
-#         # The order by is_admin='0' moves admin to the first in list
-#         # which allows sql injection
-#         users = User.objects.raw(
-#             "SELECT * FROM %s WHERE user_id='%s' ORDER BY is_admin='0'"
-#             % (table_name, user_id_form))
-#         try:
-#             user = users[0]
-#         except:
-#             return HttpResponse("User " + str(user_id_form) + " NOT FOUND")
-#         update = dict()
-#         err_msg = User.validate_update_form(form, user, update)
-#         if len(err_msg) > 0:
-#             messages.add_message(request, messages.INFO, err_msg)
-#         else:
-#             try:
-#                 # skip hash_password if password not updated
-#                 if "password" not in update:
-#                     User.objects.filter(pk=user.pk).update(**update)
-#                 else:
-#                     user.__dict__.update(update)
-#                     user.save()
-#                 messages.add_message(request, messages.INFO,
-#                                      "Successfully Updated")
-#             except Exception as e:
-#                 messages.add_message(request, messages.INFO, str(e))
-#         return redirect("/users/%s/account_settings" % user_id,
-#                         permanent=False)
+ @require_http_methods(["GET", "POST", "PUT", "DELETE"])
+ @user_is_authenticated
+ def user_view(request, user_id):
+     if request.method == "POST":
+         form = request.POST
+         if not form:
+             return HttpResponse("User " + str(user_id) + "POST")
+         user_id_form = form['user_id']
+         table_name = User.objects.model._meta.db_table
+          The order by is_admin='0' moves admin to the first in list
+          which allows sql injection
+         users = User.objects.raw(
+             "SELECT * FROM %s WHERE user_id='%s' ORDER BY is_admin='0'"
+             % (table_name, user_id_form))
+         try:
+             user = users[0]
+         except:
+             return HttpResponse("User " + str(user_id_form) + " NOT FOUND")
+         update = dict()
+         err_msg = User.validate_update_form(form, user, update)
+         if len(err_msg) > 0:
+             messages.add_message(request, messages.INFO, err_msg)
+         else:
+             try:
+                  skip hash_password if password not updated
+                 if "password" not in update:
+                     User.objects.filter(pk=user.pk).update(**update)
+                 else:
+                     user.__dict__.update(update)
+                     user.save()
+                 messages.add_message(request, messages.INFO,
+                                      "Successfully Updated")
+             except Exception as e:
+                 messages.add_message(request, messages.INFO, str(e))
+         return redirect("/users/%s/account_settings" % user_id,
+                         permanent=False)
 
-#     else:
-#         return HttpResponse("User " + str(user_id) + " " + str(request.method))
+     else:
+         return HttpResponse("User " + str(user_id) + " " + str(request.method))
